a6dc42c2c421261354aac322e04468f289460130
Adiran Szuszkiewicz <adrianszuszkiewicz@gmail.com>
2025-06-13 19:48:53 +0200

    feat: initialize Tauri app with React and TypeScript template

---
+++ flake.nix
@@ -0,0 +1,45 @@
+{
+  description = "Development shell for Tauri application";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        devShells.default = pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [
+            pkg-config
+            gobject-introspection
+            cargo
+            cargo-tauri
+            nodejs
+            rustc
+          ];
+
+          buildInputs = with pkgs; [
+            at-spi2-atk
+            atkmm
+            cairo
+            gdk-pixbuf
+            glib
+            gtk3
+            harfbuzz
+            librsvg
+            libsoup_3
+            pango
+            webkitgtk_4_1
+            openssl
+          ];
+
+          shellHook = ''
+            echo "Tauri development environment"
+            echo "Node version: $(node --version)"
+            echo "Cargo version: $(cargo --version)"
+          '';
+        };
+      });
+}
